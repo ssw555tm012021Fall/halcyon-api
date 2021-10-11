914427248769df5bda5179b67b1c616250a744ca
Viyeta <82687478+Viyeta@users.noreply.github.com>
2021-10-11 01:53:10 -0400

    download cert beforeinstall in travis

--- data/db.py
+++ data/db.py
@@ -14,10 +14,9 @@ from sqlalchemy.dialects import registry
 registry.register("cockroachdb", "cockroachdb.sqlalchemy.dialect",
                   "CockroachDBDialect")
 
-# conn_string = "cockroachdb://kavish:erKaCOuWe-zIMxPe@free-tier.gcp-us-central1.cockroachlabs.cloud:26257" \
-#                   "/second-jaguar-3728.defaultdb?sslmode=verify-full&sslrootcert=root.crt"
 conn_string = "cockroachdb://kavish:erKaCOuWe-zIMxPe@free-tier.gcp-us-central1.cockroachlabs.cloud:26257" \
-                  "/second-jaguar-3728.defaultdb?sslmode=disable"
+                  "/second-jaguar-3728.defaultdb?sslmode=verify-full&sslrootcert=root.crt"
+
 engine = create_engine(os.path.expandvars(conn_string), convert_unicode=True)
 
 session = scoped_session(sessionmaker(autocommit=False,
