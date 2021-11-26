204648c6280a32a26fa12f434719ab9b3a16c7a0
Kavish Sanghvi <sanghvi_kavish@yahoo.in>
2021-11-26 00:50:13 -0500

    issue resolved

--- data/award.py
+++ data/award.py
@@ -1,9 +1,20 @@
 import datetime
 import jwt
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, Time, Enum
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, Time
 from server import app, db
 from sqlalchemy.sql.sqltypes import BOOLEAN, CHAR, TIMESTAMP
-from data.custom_enums import  Event_State, Event_Category
+import Enum
+
+class Event_State(enum.Enum):
+    completed = 'completed'
+    cancel = 'cancel'
+
+class Event_Category(enum.Enum):
+    guided_meditation = 'guided_meditation'
+    meditation = 'meditation'
+    water = 'water'
+    break_category = 'break'
+
 
 
 class Awards(db.Model):
@@ -27,40 +38,3 @@ class Awards(db.Model):
         self.category = category
         self.frequency = frequency
         self.created_at = created_at
-        
-
-    def encode_auth_token(self, user_id):
-        """
-        Generates the Auth Token
-        :return: string
-        """
-        try:
-            payload = {
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, minutes=5),
-                'iat': datetime.datetime.utcnow(),
-                'sub': user_id
-            }
-            return jwt.encode(
-                payload,
-                app.config.get('SECRET_KEY'),
-                algorithm='HS256'
-            )
-        except Exception as e:
-            return e
-
-    @staticmethod
-    def decode_auth_token(auth_token):
-        """
-        Validates the auth token
-        :param auth_token:
-        :return: integer|string
-        """
-        try:
-            payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'), algorithms=['HS256'],
-                                 options={"verify_signature": False})
-            # TODO: check blacklisted token
-            return payload['sub']
-        except jwt.ExpiredSignatureError:
-            return 'Signature expired. Please log in again.'
-        except jwt.InvalidTokenError:
-            return 'Invalid token. Please log in again.'
--- data/employee_award.py
+++ data/employee_award.py
@@ -17,39 +17,3 @@ class EmployeeAward(db.Model):
         self.employee_id = employee_id
         self.award_id = award_id
         self.created_at = created_at
-
-    def encode_auth_token(self, user_id):
-        """
-        Generates the Auth Token
-        :return: string
-        """
-        try:
-            payload = {
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, minutes=5),
-                'iat': datetime.datetime.utcnow(),
-                'sub': user_id
-            }
-            return jwt.encode(
-                payload,
-                app.config.get('SECRET_KEY'),
-                algorithm='HS256'
-            )
-        except Exception as e:
-            return e
-
-    @staticmethod
-    def decode_auth_token(auth_token):
-        """
-        Validates the auth token
-        :param auth_token:
-        :return: integer|string
-        """
-        try:
-            payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'), algorithms=['HS256'],
-                                 options={"verify_signature": False})
-            # TODO: check blacklisted token
-            return payload['sub']
-        except jwt.ExpiredSignatureError:
-            return 'Signature expired. Please log in again.'
-        except jwt.InvalidTokenError:
-            return 'Invalid token. Please log in again.'
